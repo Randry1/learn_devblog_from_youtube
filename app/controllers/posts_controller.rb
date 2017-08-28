ea22cc762d36ef7db9d56b4003b937a399a6412c
Randry1 <revinandry@gmail.com>
2017-08-29 00:51:21 +0700

    add locale

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,9 @@ class PostsController < ApplicationController
     @post = Post.new( post_params )
 
     if @post.save
-      redirect_to @post
+      redirect_to @post, success: 'Статья создана'
     else
+      flash[:danger] = 'Статья не сохранена'
       render :new
     end
   end
@@ -28,15 +29,16 @@ class PostsController < ApplicationController
 
   def update
     if @post.update_attributes( post_params )
-      redirect_to @post
+      redirect_to @post, info: 'Статья успешно обновлена'
     else
+      flash[:danger] = 'Статься не обновлена'
       render :edit
     end
   end
 
   def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, success: 'Статья удалена'
   end
 
   def contacts
